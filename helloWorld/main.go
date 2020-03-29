49ccad04211c219a4f215a1509073e5f34b604ff
yuki2523 <2523902149@qq.com>
2020-03-29 22:07:56 +0800

    类型推导、简短变量声明、匿名变量等全写在这里面了

--- helloWorld/main.go
+++ helloWorld/main.go
@@ -12,6 +12,10 @@ var (
 	studentName string // 这种小驼峰格式的，和js一样
 )
 
+func foo() (int, string) {
+	return 10, "hahaha"
+}
+
 func main() { // 入口函数
 	// fmt.Println("Hello World")
 	name = "hcy"
@@ -23,4 +27,23 @@ func main() { // 入口函数
 	fmt.Println(age)            // 这个是默认加换行的
 	fmt.Print(isOK)             // 这个就是单纯的打印
 	fmt.Println(studentName)
+
+	// 声明的同时赋值
+	var s1 string = "yuki"
+	var a1 int = 19
+	fmt.Printf("name:%s,age:%d", s1, a1)
+
+	// 类型推导
+	var s2 = "haru" // 它可以自己根据值分析自己的类型
+	fmt.Println(s2)
+
+	// 简短变量声明
+	s3 := "Hello" // 这个就和上面的类型推导相同，不过只能在函数里面用
+	fmt.Println(s3)
+
+	// 匿名变量：_
+	// 它不占内存空间，不占命名空间，所以也不存在重复命名这回事
+	// 主要用来占位，表示忽略值
+	x, _ := foo() // 这种来只要一个值的，不要的值就让 _ 接收
+	fmt.Println(x)
 }
