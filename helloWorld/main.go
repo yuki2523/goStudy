2523c25d3b09453911ce50e03ccaa28a64ce1e91
yuki2523 <2523902149@qq.com>
2020-03-29 21:32:46 +0800

    变量命名也写在里面了

--- helloWorld/main.go
+++ helloWorld/main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import "fmt" // 导包
 
-func main() {
-	fmt.Println("Hello World")
+// 函数外面只能放一些变量常量的声明，逻辑必须放在代码块里
+
+// 声明变量：声明的名字建议是驼峰命名法
+var (
+	name        string
+	age         int
+	isOK        bool
+	studentName string // 这种小驼峰格式的，和js一样
+)
+
+func main() { // 入口函数
+	// fmt.Println("Hello World")
+	name = "hcy"
+	age = 19
+	isOK = true
+	studentName = "huachenyang"
+	// go 里变量声明了就要用，不用不给过编译
+	fmt.Printf("name:%s", name) // 加了f的是格式化输出，经典%s占位符
+	fmt.Println(age)            // 这个是默认加换行的
+	fmt.Print(isOK)             // 这个就是单纯的打印
+	fmt.Println(studentName)
 }
